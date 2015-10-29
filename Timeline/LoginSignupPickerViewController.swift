a7199f3559e10e79d4203423ce11335259c9a2df
Caleb Hicks <calebhicks@gmail.com>
2015-10-29 17:06:26 -0600

    part2: login flow (partially in previous commit)

--- Timeline/LoginSignupPickerViewController.swift
+++ Timeline/LoginSignupPickerViewController.swift
@@ -22,14 +22,21 @@ class LoginSignupPickerViewController: UIViewController {
     }
     
 
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if let destinationViewController = segue.destinationViewController as? LoginSignupViewController {
+            
+            if segue.identifier == "toLogin" {
+                destinationViewController.mode = .Login
+            }
+            
+            if segue.identifier == "toSignup" {
+                destinationViewController.mode = .Signup
+            }
+        }
     }
-    */
-
+    
 }
